57131a2b5782a810bfa42f1133bc2c315dad2d77
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-26 09:15:18 +0530

    update orders.dart

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,64 +1,59 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class MyOrders extends StatelessWidget {
   const MyOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Orders'),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: 300,
-            margin: EdgeInsets.all(30),
-            padding: EdgeInsets.all(40),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image:AssetImage('images/cart.jpg'),
-                fit: BoxFit.cover
-              ),
-              shape: BoxShape.circle
+    return SafeArea(
+        child: CupertinoPageScaffold(
+            navigationBar: const CupertinoNavigationBar(
+              middle: Text('My Orders'),
             ),
-          ),
-          const Center(
-            child: Text("No orders yet!",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 24,
-              fontFamily: 'Verdana',
-              color: Colors.redAccent,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 12,
-                  backgroundColor: Colors.deepOrange,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
+            child: Column(
+              children: [
+                Container(
+                  height: 300,
+                  width: 300,
+                  margin: EdgeInsets.all(60),
+                  padding: EdgeInsets.all(40),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('images/cart.jpg'),
+                          fit: BoxFit.cover),
+                      shape: BoxShape.circle),
+                ),
+                const Center(
+                  child: Text(
+                    "No orders yet!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Verdana',
+                        color: CupertinoColors.systemRed,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.all(14),
-                  child: Text('Continue Shopping!',
-                  style: TextStyle(fontSize: 20, color: Colors.white),),
-                ),
-                onPressed: (){
-                  Navigator.pushNamed(context, '/categories');
-                },
-              )
-            ),
-          )
-        ],
-      )
-    );
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                      child: CupertinoButton.filled(
+                    child: const Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        'Continue Shopping!',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: CupertinoColors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/categories');
+                    },
+                  )),
+                )
+              ],
+            )));
   }
 }
